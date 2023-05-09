4d3b3ffcf7be7f80bfb00875f42041d6c33aa6e2
nhanhuynh123 <91048745+nhanhuynh123@users.noreply.github.com>
2023-05-09 09:18:18 +0700

    Update time.cpp

--- OOP_week9/time.cpp
+++ OOP_week9/time.cpp
@@ -16,3 +16,14 @@ public:
 
 // dung static:
 // muon tinh toan, xu ly 1 tien ich hoac van de ko lien quan den class hien tai: tim 'uoc chung lon nhat' trong 'ham rut gon' cua class 'phan so'
+
+
+// Ham huy destructor: dung de huy doi tuong
+// giong voi constructor:
+// ten ham trung voi ten lop
+// khac voi constructor:
+// co dau nga phia truoc
+// chi co duy nhat mot ham huy, nhung co nhieu constructor
+// neu ko dinh nghia ham huy thi mac nhien compiler se tu tao 1 ham huy tu nhien
+// ham huy tu dong duoc goi
+// khi ko co thuoc tinh con tro ko can build ham huy
