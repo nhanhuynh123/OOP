f51c4aecd720353bd6f87815fda4174d439f5f84
nhanhuynh123 <91048745+nhanhuynh123@users.noreply.github.com>
2023-06-06 00:22:32 +0700

    Create HOTEL2.cpp

---
+++ HOTEL2.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+class Hotel;
+class room{
+protected:
+    int So_dem = 0;
+public:
+    virtual void Nhap(){
+        cin >> So_dem;
+    }
+    virtual float dThu(){
+        return 0;
+    }
+};
+class Deluxe : public room{
+    static float TongdThu;
+    float pdv = 0.0, ppv = 0.0;
+public:
+    void Nhap(){
+        room::Nhap();
+        cin >> pdv >> ppv;
+    }
+    float dThu(){
+        TongdThu += pdv + ppv + 750 * So_dem;
+        return TongdThu;
+    }
+    static float Tong(){ return TongdThu;}
+};
+class Premium : public room{
+    static float TongdThu;
+    float pdv;
+    public:
+    void Nhap(){
+        room::Nhap();
+        cin >> pdv;
+    }
+    float dThu(){
+        TongdThu += pdv + 500 * So_dem;
+        return pdv + 500 * So_dem;
+    }
+    static float Tong(){ return TongdThu;}
+};
+class Business : public room{
+    static float TongdThu;
+    public:
+    float dThu(){
+        TongdThu += 300 * So_dem;
+        return 300 * So_dem;
+    }
+    static float Tong(){ return TongdThu;}
+};
+float Deluxe::TongdThu = 0;
+float Premium::TongdThu = 0;
+float Business::TongdThu = 0;
+class Hotel{
+    int iN;
+    room* arr[50];
+public:
+    friend istream& operator>> (istream &is, Hotel &x){
+        is >> x.iN;
+        for(int i = 0; i < x.iN; i++){
+            int k; is >> k;
+            switch(k) {
+                case 1:
+                    x.arr[i] = new Deluxe;
+                    x.arr[i]->Nhap();
+                    break;
+                case 2:
+                    x.arr[i] = new Premium;
+                    x.arr[i]->Nhap();
+                    break;
+                case 3:
+                    x.arr[i] = new Business;
+                    x.arr[i]->Nhap();
+                    break;
+            }
+        }
+        for(int i = 1; i < x.iN; i++) x.arr[i]->dThu();
+        return is;
+    }
+    int find_max_dThu(){
+        if(Deluxe::Tong() > Business::Tong()){
+            if(Deluxe::Tong() > Premium::Tong()) return 1;
+            return 2;
+        }
+        else{
+            if(Business::Tong() > Premium::Tong()) return 3;
+            return 2;
+        }
+    }
+};
+int main(){
+    Hotel Sofitel;
+    cin >> Sofitel;
+    cout << Sofitel.find_max_dThu();
+    return 0;
+}
